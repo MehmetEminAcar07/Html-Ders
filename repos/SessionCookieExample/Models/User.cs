125afde0a1459a3fbf1a51007594a2bbfa06d985
Mehmet Emin Acar <149245927+MehmetEminAcar07@users.noreply.github.com>
2025-12-18 12:32:32 +0300

    Delete repos directory

--- repos/SessionCookieExample/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SessionCookieExample.Models
-{
-    public class User
-    {
-        public string UserName { get; set; }
-        public string UserPass { get; set; }
-        public string UserColor { get; set; }
-    }
-}

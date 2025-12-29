00ebb3dfa163d3d7924a4906dd4c135862107894
MehmetEminAcar07 <MehmetEminAcar07@example.com>
2025-12-29 16:39:27 +0300

    js silme

--- repos/JS dersleri/App_Start/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace App_Start.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
